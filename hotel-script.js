9b54fa2ea7613cb448c8b9d102930b06ae63875b
SezerALAGOZ <49189088+SezerALAGOZ@users.noreply.github.com>
2019-08-09 22:19:37 +0300

    style changes made

--- hotel-script.js
+++ hotel-script.js
@@ -16,13 +16,13 @@ function DisplayMenu() {
     document.getElementById('side-nav').style.display = 'grid';
     document.getElementById('side-nav').style.gridTemplateColumns = '1fr';
     document.getElementById('side-nav').style.gridTemplateRows = 'auto auto auto auto auto auto auto';
-    document.getElementById('side-nav').style.gridRowGap = '5vh';
+    document.getElementById('side-nav').style.gridRowGap = '2vh';
     document.getElementById('side-nav').style.position = 'fixed';
     document.getElementById('side-nav').style.width = '70%';
     document.getElementById('side-nav').style.height = '100vh';
     document.getElementById('side-nav').style.background = '#211c1c';
     document.getElementById('side-nav').style.zIndex = '1';
-    document.getElementById('side-nav').style.padding = '10% 5%';
+    document.getElementById('side-nav').style.padding = '6% 4%';
 }
 
 function DisplayRooms() {
